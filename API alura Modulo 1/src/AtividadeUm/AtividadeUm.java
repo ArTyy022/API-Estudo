8becddf5b4afac27194bd5fc9587b3b8b5a89ca7
ArTyy022 <toledoarthur11@gmail.com>
2025-09-24 11:22:47 -0300

    Add files via upload

---
+++ API alura Modulo 1/src/AtividadeUm/AtividadeUm.java
@@ -0,0 +1,33 @@
+package AtividadeUm;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class AtividadeUm {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um livro: ");
+        String busca = leitura.nextLine();
+
+
+        String chave = "&apikey=84b61422";
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + busca + "&key=" + chave;
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString() );
+
+        System.out.println(response.body());
+
+    }
+}
+
