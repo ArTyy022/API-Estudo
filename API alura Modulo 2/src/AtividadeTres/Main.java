ae29ed3cb0dc60a0e6351eed973b783ad4b80356
ArTyy022 <toledoarthur11@gmail.com>
2025-10-06 11:08:54 -0300

    Add files via upload

--- API alura Modulo 2/src/AtividadeTres/Main.java
+++ API alura Modulo 2/src/AtividadeTres/Main.java
@@ -1,4 +1,15 @@
 package AtividadeTres;
 
+import com.google.gson.Gson;
+
 public class Main {
+
+    public static void main(String[] args) {
+        String jsonLivro = "{\"titulo\":\"Dom Casmurro!\n\",\"autor\":\"Machado de Assis\n\",\"editora\":{\"nome\":\"Livraria Garnier\",\"cidade\":\"Rio de Janeiro\"}}";
+
+        Gson gson = new Gson();
+        Livro livro = gson.fromJson(jsonLivro, Livro.class);
+
+        System.out.println("Livro: " + livro);
+    }
 }
