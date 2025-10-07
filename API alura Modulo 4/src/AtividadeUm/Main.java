fcff4de56df3d5f9ecfae952bddf513bf25be2fd
ArTyy022 <toledoarthur11@gmail.com>
2025-10-07 20:41:40 -0300

    Add files via upload

--- API alura Modulo 4/src/AtividadeUm/Main.java
+++ API alura Modulo 4/src/AtividadeUm/Main.java
@@ -1,7 +1,15 @@
 package AtividadeUm;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String conteudo = "Conteúdo a ser gravado no arquivo.";
+
+        FileWriter escrita = new FileWriter("arquivo.txt");
+        escrita.write(conteudo);
+        escrita.close();
     }
 }
