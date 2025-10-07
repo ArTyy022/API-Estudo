7d382f9c3fe55f7c3544441c7c9aa4f1abb82184
ArTyy022 <toledoarthur11@gmail.com>
2025-10-07 12:07:35 -0300

    Add files via upload

--- API alura Modulo 3/src/AtividadeDois/Main.java
+++ API alura Modulo 3/src/AtividadeDois/Main.java
@@ -1,4 +1,26 @@
 package AtividadeDois;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua senha: ");
+        String senha = scanner.nextLine();
+
+        try {
+            validarSenha(senha);
+            System.out.println("Senha válida. Acesso permitido.");
+        } catch (SenhaInvalida e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    private static void validarSenha(String senha) {
+        if (senha.length() < 8) {
+            throw new SenhaInvalida("A senha deve ter pelo menos 8 caracteres.");
+        }
+    }
 }
