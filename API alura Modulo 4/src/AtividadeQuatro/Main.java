86939bb7b25295101f33a448024918d5f6c9c931
ArTyy022 <toledoarthur11@gmail.com>
2025-10-07 20:55:42 -0300

    Add files via upload

--- API alura Modulo 4/src/AtividadeQuatro/Main.java
+++ API alura Modulo 4/src/AtividadeQuatro/Main.java
@@ -1,4 +1,22 @@
 package AtividadeQuatro;
 
+import com.google.gson.Gson;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+ Veiculo veiculo = new Veiculo();
+ veiculo.setNome("Lancer");
+ veiculo.setModelo("Mitsubishi");
+ veiculo.setAnoDeFabricacao(2012);
+ veiculo.setPreco(80.000);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(veiculo);
+
+        System.out.println(json);
+
+
+    }
 }
---
+++ API alura Modulo 4/src/AtividadeQuatro/Veiculo.java
@@ -0,0 +1,41 @@
+package AtividadeQuatro;
+
+public class Veiculo {
+    private String nome;
+    private String modelo;
+    private int anoDeFabricacao;
+    private double preco;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnoDeFabricacao() {
+        return anoDeFabricacao;
+    }
+
+    public void setAnoDeFabricacao(int anoDeFabricacao) {
+        this.anoDeFabricacao = anoDeFabricacao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
